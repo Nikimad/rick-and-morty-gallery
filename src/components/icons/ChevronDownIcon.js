99b81e4239a0e784a93dea83fa8aae9be1585d1e
Nikita Savelev <pybepod@gmail.com>
2025-07-21 12:21:46 +0300

    Add src/components/icons: chevrons and close icons

---
+++ src/components/icons/ChevronDownIcon.js
@@ -0,0 +1,5 @@
+import { ReactComponent as ChevronDown } from '../../assets/common/chevron-down.svg';
+
+export const ChevronDownIcon = () => (
+  <ChevronDown width={20} height={20} aria-hidden="true" />
+);
