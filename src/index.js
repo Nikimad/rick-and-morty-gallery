fc8a6dc1cc3c757892cdba3e017e8c3b5f5e30af
Nikita Savelev <pybepod@gmail.com>
2025-07-21 12:26:06 +0300

    Change src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { DataProvider, InertProvider } from './components';
+import { Providers } from './components';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <InertProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </InertProvider>
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
